636b948ee610389823fb177a67f21bf0bd68030f
SinanJS <titan_js@163.com>
2017-05-19 20:10:51 +0800

    单向数据流绑定，初步

--- dist/ti.js
+++ dist/ti.js
@@ -259,15 +259,37 @@ define('dom/velement',["../core"],function (Ti) {
 define('dom/directive',[
     "../core",
     "./velement"
-], function (Ti,VElement) {
+], function (Ti, VElement) {
     var drtUtils = {
-        _data:{},
+        _data: {},
         prefix: "t",
+        readData: function (data, key) {
+            if (typeof key !== "string") {
+                return console.error("props must be string");
+            }
+            var arr = key.split(".");
+            var result;
+            if (arr.length > 1) {
+                result = data;
+                for(var i =0;i<arr.length;i++){
+
+                    result = result[arr[i]];
+                }
+            } else {
+                if(/\[/.test(key)){
+                    //如果key代表一个数组
+
+                }else{
+                    result = data[key];
+                }
+            }
+            return result;
+        },
         directives: {
-            text: function (children,prop) {
-                var text = drtUtils._data[prop].toString();
-                console.log(text)
-                var vEl = new VElement("#text",[],{},text);
+            text: function (children, prop) {
+                var text = drtUtils.readData(drtUtils._data,prop);
+                console.log("12",text);
+                var vEl = new VElement("#text", [], {}, text);
                 return [vEl];
             }
         },
